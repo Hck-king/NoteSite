301202b1f2f577ef3a554123f0f9d73c68d7f113
Hck-king <72938430+Hck-king@users.noreply.github.com>
2025-07-12 18:25:37 +0530

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 # Set this to your domain or server IP in production
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
